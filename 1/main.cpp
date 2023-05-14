969e4851745e7ee02c3ab9772d181deae4fb72a6
aXXo-dev <58981352+aXXo-dev@users.noreply.github.com>
2023-05-14 17:20:45 -0400

    Initial Commit

---
+++ 1/main.cpp
@@ -0,0 +1,46 @@
+#include <thread>
+#include <cstdio>
+
+#include <Windows.h>
+#include <jni.h>
+
+void MainThread(HMODULE instance)
+{
+    JavaVM* p_jvm{ nullptr };
+    jint result = JNI_GetCreatedJavaVMs(&p_jvm, 1, nullptr);
+
+    void* p_env{ nullptr }; // JNI interface pointer
+    JavaVMAttachArgs attach_args;
+    attach_args.version = JNI_VERSION_10;
+    p_jvm->AttachCurrentThread(&p_env, &attach_args);
+
+    while (!GetAsyncKeyState(VK_END))
+    {
+
+    }
+
+    FreeLibrary(instance);
+}
+
+bool __stdcall DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) 
+{
+    static FILE* p_file{ nullptr };
+    static std::thread main_thread;
+
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        AllocConsole();
+        freopen_s(&p_file, "CONOUT$", "w", stdout);
+
+        main_thread = std::thread([instance] { MainThread(instance); });
+        if (main_thread.joinable())
+            main_thread.detach();
+    }
+    else if (reason == DLL_PROCESS_DETACH)
+    {
+        fclose(p_file);
+        FreeConsole();
+    }
+
+    return true;
+}
